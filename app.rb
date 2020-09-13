7873a4635d67aff1961f7bd1cb9e1d3c359a8def
Ryna Truong <rynatruong@outlook.com>
2020-09-13 21:06:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
   end
   
   get '/say/:number' do
-    @number = params[:number]
+    @number = params[:number].to_i
     @phrase = params[:phrase]
     @phrase.times(@number)
   end
