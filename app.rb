0692a325a15b63cff350ec183481d634d1d2c653
Ryna Truong <rynatruong@outlook.com>
2020-09-13 21:11:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -28,6 +28,8 @@ class App < Sinatra::Base
   end
   
   get '/:operation/:number1/:number2' do
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
     if params[:operation] == "add"
       @operation = :+
     elsif params[:operation] == "subtract"
