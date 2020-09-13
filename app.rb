0a8d72d25191c7e04b0988a04b606954280aa223
Ryna Truong <rynatruong@outlook.com>
2020-09-13 21:10:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -26,4 +26,7 @@ class App < Sinatra::Base
     @word5 = params[:word5]
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
+  
+  get '/:operation/:number1/:number2' do
+    @operation =
 end
