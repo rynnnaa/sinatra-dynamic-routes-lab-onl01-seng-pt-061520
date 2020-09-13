9a0f7323cf29956349652ed8d7de2d05ead7d776
Ryna Truong <rynatruong@outlook.com>
2020-09-13 21:01:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,4 +6,11 @@ class App < Sinatra::Base
     @name = params[:name]
     @name.reverse
   end
+  
+  get '/square/:number'
+    do
+      @num = params[:number].to_i
+      @num ** 2 
+    end
+    
 end
